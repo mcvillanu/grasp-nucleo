967329833f34427a5ca304d835a4aadd81aa9af6
raunakbhagat <n1z1b>
2020-02-10 21:23:46 -0800

    am going to try abstracting certain things away. If it doesn't work, I'll come back to this branch lol

--- src/Utilities/JSON/JSON.h
+++ src/Utilities/JSON/JSON.h
@@ -8,7 +8,6 @@ class JSON {
     private:
         int size;
         DynamicJsonDocument * json;
-        JsonObject const * const obj;
         DeserializationError error;
 
         bool const fix_error();
@@ -18,13 +17,16 @@ class JSON {
         JSON(JSON const & other);
         ~JSON();
 
-        void operator=(JSON const & other);
-        bool const operator==(JSON const & other) const;
-        bool const operator!=(JSON const & other) const;
+        void       operator =  (JSON const & other);
+        bool const operator == (JSON const & other) const;
+        bool const operator != (JSON const & other) const;
 
         bool           const start  (String const * const & str);
         String const * const finish ();
 
+        bool const containsKey(String const & key) const;
+
+        int                         const getValueType (String const & key)          const;
         template<class T> T const * const getValue     (String const & key)          const;
         template<class T> bool      const setValue     (String const & key, T const & val);
         template<class T> bool      const replaceValue (String const & key, T const & val);
