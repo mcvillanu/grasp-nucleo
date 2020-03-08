dec8615b91601842c67ec85eaa7f9dcf89a94364
James <james.rosstwo@gmail.com>
2020-03-07 16:18:47 -0800

    add fsr matrix

---
+++ src/FSRMatrix/fsrMatrix.cpp
@@ -0,0 +1,93 @@
+#include "fsrMatrix.h";
+#include <Arduino.h>;
+#include <constants.h>;
+
+FSRMatrix::FSRMatrix(){
+    initPins();
+}
+
+void FSRMatrix::initPins(){
+    pinMode(PINS::PIN_ADC_INPUT, INPUT);
+    pinMode(PINS::PIN_SHIFT_REGISTER_DATA, OUTPUT);
+    pinMode(PINS::PIN_SHIFT_REGISTER_CLOCK, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_0, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_1, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_2, OUTPUT);
+    pinMode(PINS::PIN_MUX_INHIBIT_0, OUTPUT);
+    pinMode(PINS::PIN_MUX_INHIBIT_1, OUTPUT);
+    pinMode(PINS::PIN_ADC_INPUT, INPUT);
+    pinMode(PINS::PIN_SHIFT_REGISTER_DATA, OUTPUT);
+    pinMode(PINS::PIN_SHIFT_REGISTER_CLOCK, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_0, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_1, OUTPUT);
+    pinMode(PINS::PIN_MUX_CHANNEL_2, OUTPUT);
+    pinMode(PINS::PIN_MUX_INHIBIT_0, OUTPUT);
+    pinMode(PINS::PIN_MUX_INHIBIT_1, OUTPUT);
+}
+
+void FSRMatrix::setRow(int rowNumber)
+{
+    //everything inside this if statement is to switch from one mux to another
+    if ((rowNumber % ROWS_PER_MUX) == 0) //We've reached channel 0 of a mux IC, so disable the previous mux IC, and enable the next mux IC
+    {
+        digitalWrite(PINS::PIN_MUX_INHIBIT_0 + current_enabled_mux, HIGH); //Muxes are enabled using offset from MUX_INHIBIT_0. This is why mux inhibits MUST be wired to consecutive Arduino pins!
+        current_enabled_mux++;
+        if (current_enabled_mux >= MUX_COUNT)
+        {
+            current_enabled_mux = 0;
+        }
+        digitalWrite(PINS::PIN_MUX_INHIBIT_0 + current_enabled_mux, LOW); //enable the next mux, active low
+    }
+    //this for loop is to iterate through the 8 pins on the current multiplexer is called in the main loop to iterate through the rows
+    for (int i = 0; i < CHANNEL_PINS_PER_MUX; i++)
+    {
+        //x: the number from which to read.
+        //n: which bit to read, starting at 0 for the least-significant (rightmost) bit.
+        //bitRead(x , n)
+        if (bitRead(rowNumber, i))
+        {
+            digitalWrite(PINS::PIN_MUX_CHANNEL_0 + i, HIGH);
+        }
+        else
+        {
+            digitalWrite(PINS::PIN_MUX_CHANNEL_0 + i, LOW);
+        }
+    }
+}
+
+void FSRMatrix::shiftColumn(bool isFirst)
+{
+    if (isFirst)
+    {
+        digitalWrite(PINS::PIN_SHIFT_REGISTER_DATA, HIGH);
+    }
+    digitalWrite(PINS::PIN_SHIFT_REGISTER_CLOCK, HIGH);
+    digitalWrite(PINS::PIN_SHIFT_REGISTER_CLOCK, LOW);
+    if (isFirst)
+    {
+        digitalWrite(PINS::PIN_SHIFT_REGISTER_DATA, LOW);
+    }
+}
+
+void FSRMatrix::read(){
+    for (int i = 0; i < FSR_MATRIX_ROW_COUNT; i++)
+    {
+        //read at the current row, starting at zero
+        setRow(i);
+        shiftColumn(true);
+        shiftColumn(false); //with SR clks tied, latched outputs are one clock behind
+
+        for (int j = 0; j < FSR_MATRIX_COLUMN_COUNT; j++)
+        {
+            //read at the current column starting 0, and iterate through all columns before skipping to the next row
+            int rawReading = analogRead(PINS::PIN_ADC_INPUT);
+
+            //shift columns until we get to the next, just sets the clock and then low to keep shifting the bit
+            shiftColumn(false);
+
+            Serial.print(rawReading);
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
+}
---
+++ src/FSRMatrix/fsrMatrix.h
@@ -0,0 +1,50 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+class FSRMatrix {
+    public:
+        const int FSR_MATRIX_ROW_COUNT = 10;
+        const int FSR_MATRIX_COLUMN_COUNT = 16;
+        const int ROWS_PER_MUX = 8;
+        const int MUX_COUNT = 2;
+        const int CHANNEL_PINS_PER_MUX = 3;
+
+        /**
+         * Default constructor for the FSR Matrix.
+         **/
+        FSRMatrix();
+
+        /**
+         * Reads the entire FSR matrix and transfers the values via serial connection
+         **/
+    
+        void read();
+    private:
+        int numReadings = 500;                   //keeps track of how many readings you'd like to take before writing the file.
+        int readingCounter = 0;                  //c
+        int current_enabled_mux = MUX_COUNT - 1; //init to number of last mux so enabled mux increments to first mux on first scan.
+
+
+        /*
+        * Function: shiftColumn
+        * ----------------------------
+        *   parameter1: boolean value that indicates transition to next shift register IC
+        *   returns: void
+        */
+        void shiftColumn(bool isFirst);
+
+        /*
+        * Function: setRow
+        * ----------------------------
+        *   parameter1: the row number
+        *   returns: void
+        */
+        void setRow(int rowNumber);
+
+        /**
+         * Sets all FSR matrix pins to desired inputs and outputs
+         **/
+        void initPins();
+
+};
+#endif
